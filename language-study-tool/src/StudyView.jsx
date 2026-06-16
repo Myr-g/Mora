c8b8037ac7ec905b1f5db63d4b5e3d68ca722062
cbgalileo <cbrice@plu.edu>
2026-06-16 10:18:00 -0700

    implemented multiple choice study mode

--- language-study-tool/src/StudyView.jsx
+++ language-study-tool/src/StudyView.jsx
@@ -9,6 +9,25 @@ function StudyView({ selectedDeck, setIsStudying })
     const [side, setSide] = useState("front");
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
+    const [choices, setChoices] = useState([]);
+
+    useEffect(() => {
+        if(studyMode === "multiple-choice" && currentCardIndex < selectedDeck.cards.length)
+        {
+            setChoices(generateChoices());
+        }
+    }, [currentCardIndex, studyMode]);
+
+    const [answers, setAnswers] = useState([]); 
+
+    // Reset State
+    const resetStudyState = () => {
+        setCurrentCardIndex(0);
+        setSide("front");
+        setChoices([]);
+        setAnswers([]);
+    };
+
     // Flashcard Functions
     const previousCard = () => {
         setCurrentCardIndex(i => i > 0 ? i - 1 : i);
@@ -24,68 +43,143 @@ function StudyView({ selectedDeck, setIsStudying })
         setSide(side => (side === "front" ? "back" : "front"));
     };
 
-    if(showStudyModal)
-    {
-        return (
-            <>
-                <section className='study-view-header'>
-                    <h1>{selectedDeck.name} - Study</h1>
-                </section>
+    // Multiple Choice Functions
+    const generateChoices = () => {
+        const correctAnswer = selectedDeck.cards[currentCardIndex].back;
 
-                <div className='study-modal'>
-                    <div className='study-modal-header'>
-                        <h2>Study Options</h2>
-                    </div>
+        let incorrectAnswers = shuffle(selectedDeck.cards.filter((_, i) => i !== currentCardIndex));
+        incorrectAnswers = incorrectAnswers.slice(0, 3).map(card => card.back);
 
-                    <div className='study-modal-options'>
-                        <label>Study Mode</label>
-                        <select className="study-modal-select" onChange={(e) => {
-                                setStudyMode(e.target.value);
-                            
-                                if(e.target.value)
-                                {
-                                    setShowStudyModal(false);
-                                }
-                            }} defaultValue={studyMode}>
-                            <option value="">Select a Study Mode</option>
-                            <option value="flashcards">Flashcards</option>
-                            <option value="multiple-choice">Multiple Choice</option>
-                            <option value="matching">Matching / Drag-and-Drop</option>
-                            <option value="write-the-definition">Write the Definition</option>
-                        </select>
-                    </div>
-
-                    <div className='study-modal-actions'>
-                        <button onClick={() => setIsStudying(false)}>Exit</button>
-                        <button onClick={() => setShowStudyModal(false)}>Cancel</button>
-                    </div>
-                </div>
-            </>
-        )
+        const choices = shuffle([correctAnswer, ...incorrectAnswers]);
+        return choices;
     }
 
-    if(studyMode === "flashcards")
-    {
-        return (
-            <>
-                <section className='study-view-header'>
-                    <h1>{selectedDeck.name} - Flashcards</h1>
-                </section>
+    const shuffle = (array) => {
+        const arr = [...array];
+        for(let i = arr.length - 1; i > 0; i --)
+        {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
 
-                <button onClick={() => setShowStudyModal(true)}>Study Options</button>
+        return arr;
+    }
 
-                <p className="card-counter">Card {currentCardIndex + 1} of {selectedDeck.cards.length}</p>
+    const handleChoice = (choice) => {
+        const correctAnswer = selectedDeck.cards[currentCardIndex].back;
 
-                <div className='flashcards'>
-                    <button className='previous-card' onClick={previousCard}>←</button>
+        setAnswers(answers => [
+            ...answers, {
+                index: currentCardIndex,
+                question: selectedDeck.cards[currentCardIndex].front,
+                selected: choice,
+                correct: correctAnswer
+            }
+        ]);
+
+        setCurrentCardIndex(i => i + 1);
+    };
+
+    return (
+        <>
+            {studyMode === "flashcards" && (
+                <>
+                    <section className='study-view-header'>
+                        <h1>{selectedDeck.name} - Flashcards</h1>
+                    </section>
+
+                    <button className='study-button' onClick={() => setShowStudyModal(true)}>Study Options</button>
+
+                    <p className="card-counter">Card {currentCardIndex + 1} of {selectedDeck.cards.length}</p>
+
+                    <div className='flashcards'>
+                        <button className='previous-card' onClick={previousCard}>←</button>
                     
-                    <div className='flashcard' onClick={flipCard}>{selectedDeck.cards[currentCardIndex][side]}</div>
+                        <div className='flashcard' onClick={flipCard}>{selectedDeck.cards[currentCardIndex][side]}</div>
 
-                    <button className='next-card' onClick={nextCard}>→</button>
-                </div>
-            </>
-        )
-    }
+                        <button className='next-card' onClick={nextCard}>→</button>
+                    </div>
+                </>
+            )}
+
+            {studyMode === "multiple-choice" && (
+                <>
+                    <section className='study-view-header'>
+                        <h1>{selectedDeck.name} - Multiple Choice</h1>
+                    </section>
+
+                    <button className='study-button' onClick={() => setShowStudyModal(true)}>Study Options</button>
+
+                    {currentCardIndex !== selectedDeck.cards.length && (
+                        <>
+                            <div className='multiple-choice'>
+                                <div className='multiple-choice-question'>
+                                    <h2>{selectedDeck.cards[currentCardIndex].front}</h2>
+                                </div>
+
+                                <div className='multiple-choice-answers'>
+                                    {choices.map((choice, index) => (
+                                        <button key={index} className='multiple-choice-option' onClick={() => handleChoice(choice)}>
+                                            {choice}
+                                        </button>
+                                    ))}
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    {currentCardIndex === selectedDeck.cards.length && (
+                        <>
+                            <p className="card-counter">Score: {answers.filter(a => a.selected === a.correct).length} / {answers.length}</p>
+
+                            {answers.map((answer, index) => (
+                                <div key={index} className={`multiple-choice ${answer.selected === answer.correct ? "correct" : "incorrect"}`}>
+                                    <div className='multiple-choice-question'>
+                                        <h2>{answer.question}</h2>
+                                    </div>
+
+                                    <div className='multiple-choice-answers'>
+                                        <p>Your Answer: {answer.selected}</p>
+                                        <p>Correct Answer: {answer.correct}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </>
+                    )}
+                </>
+            )}
+
+            {showStudyModal && (
+                <>
+                    <div className='study-modal-overlay'>
+                        <div className='study-modal'>
+                            <div className='study-modal-header'>
+                                <h2>Study Options</h2>
+                            </div>
+
+                            <div className='study-modal-options'>
+                                <label>Study Mode</label>
+                                <select className="study-modal-select" onChange={(e) => {if(e.target.value) { resetStudyState(); setStudyMode(e.target.value); setShowStudyModal(false); }}} defaultValue={studyMode}>
+                                    <option value="">Select a Study Mode</option>
+                                    <option value="flashcards">Flashcards</option>
+                                    <option value="multiple-choice">Multiple Choice</option>
+                                    <option value="matching">Matching / Drag-and-Drop</option>
+                                    <option value="write-the-definition">Write the Definition</option>
+                                </select>
+                            </div>
+
+                            <div className='study-modal-actions'>
+                                <button onClick={() => setIsStudying(false)}>Exit</button>
+                                <button onClick={() => {setShowStudyModal(false); if(!studyMode) { setIsStudying(false); }}}>Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
+    )
 }
 
 export default StudyView;
