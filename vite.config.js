b652bb2c09266d126824906e606f392b9ea56c74
cbgalileo <cbrice@plu.edu>
2026-06-26 15:30:36 -0700

    fixed manifest issue

--- vite.config.js
+++ vite.config.js
@@ -8,31 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        "name": "Mora",
-        "short_name": "Mora",
-        "start_url": "/Mora/",
-        "scope": "/Mora/",
-        "display": "standalone",
-        "background_color": "#121212",
-        "theme_color": "#4A5563",
-        "icons": [
-          {
-            "src": "/Mora/icons/icon-192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "/Mora/icons/icon-512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
-        ]
-      },
-      workbox: {
-        navigateFallback: '/Mora/index.html'
-      },
+      registerType: "autoUpdate"
     })
   ],
 })
